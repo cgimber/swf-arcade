a15cd638b56211932ef5f5f0e62e4ef98b77b59a
Christian Gimber <cgimber@ucla.edu>
2016-07-30 00:08:40 -0700

    SWFObject, loading callback, and error message

--- js/arcade.js
+++ js/arcade.js
@@ -1,7 +1,9 @@
 /* globals
 ---------------------------------------------------------------------*/
 var numStudent = 0;
+var numPrev, numNext;
 var hash = '';
+var loadCheckInterval;
 
 /* document ready
 ---------------------------------------------------------------------*/
@@ -26,7 +28,7 @@ function checkHash() {
     if (location.hash === '')
         location.hash = '#0';
     hash = location.hash;
-    numStudent = hash.substring(1);
+    numStudent = parseInt(hash.substring(1));
 }
 
 function updatePage() {
@@ -34,44 +36,86 @@ function updatePage() {
     var currStudent = students[numStudent];
     var fullName = currStudent.firstname + ' ' + currStudent.lastname;
     var pathToGame = 'swf/' + currStudent.firstname + '-' + currStudent.lastname + '.swf';
+    var params = { quality: "autohigh" };
+    var callback = function(e) {
+        // only execute if SWFObject embed was successful
+        if (!e.success || !e.ref) {
+            $('.game__alert--loading').hide();
+            $('.game__alert--error').show();
+            return false;
+        }
 
+        var count = 0;
+
+        // this timeout ensures we don't try to access PercentLoaded too soon
+        var initialTimeout = setTimeout(function() {
+            // ensure Flash Player's PercentLoaded method is available and returns a value
+            if (typeof e.ref.PercentLoaded !== "undefined" && e.ref.PercentLoaded()) {
+                // set up a timer to periodically check value of PercentLoaded
+                loadCheckInterval = setInterval(function() {
+                    if (e.ref.PercentLoaded() === 100) {
+                        e.ref.focus();
+                        $('.game__alert--loading').hide();
+                        $('.game__alert--click').show();
+                        $('.game__alert').fadeOut('1000');
+                        clearInterval(loadCheckInterval);
+                    } else {
+                        // animate loading dots
+                        count++;
+                        var dots = new Array(count % 5).join('.');
+                        $('.game__alert--loading').text("loading" + dots);
+                    }
+                }, 500);
+            } else {
+                $('.game__alert--loading').hide();
+                $('.game__alert--click').show();
+                $('.game__alert').fadeOut('1000');
+                clearInterval(loadCheckInterval);
+            }
+        }, 200);
+    };
+
+    if (loadCheckInterval)
+        clearInterval(loadCheckInterval);
+    
     // game
     $('.game__alert')
         .width(currStudent.width)
         .height(currStudent.height)
         .show();
-    $('#game').replaceWith("<object id='game' name='game' type='application/x-shockwave-flash' data='" + pathToGame + "' width='" + currStudent.width + "' height='" + currStudent.height + "'> < param name = 'movie' value = '" + pathToGame + "'/> <param name = 'quality' value = 'autohigh' / > </object>");
+    $('.game__alert--loading').show();
+    $('.game__alert--click').hide();
+    $('.game__alert--error').hide();
+
+    swfobject.embedSWF(pathToGame, 
+                       "game", 
+                       currStudent.width, 
+                       currStudent.height, 
+                       9, 
+                       false, // express install
+                       false, // flashvars 
+                       params, 
+                       false, // attributes 
+                       callback);
+
     $('.game__title').text(fullName);
-    watchSwf($('#game'));
 
     // document
     document.title = 'swf-arcade - ' + fullName;
 
-    // links
+    // determine the index of the prev/next students
     if (numStudent > 0)
-        $('.control__prev-game').prop('href', '#' + (parseInt(numStudent) - 1));
-    else
-        $('.control__prev-game').prop('href', '#' + (parseInt(students.length) - 1));
+        numPrev = numStudent - 1;
+    else numPrev = students.length - 1;
 
     if (numStudent < students.length - 1)
-        $('.control__next-game').prop('href', '#' + (parseInt(numStudent) + 1));
-    else
-        $('.control__next-game').prop('href', '#0');
-    $('.control__reload-game').prop('href', '#' + parseInt(numStudent));
+        numNext = numStudent + 1;
+    else numNext = 0;
+
+    // links
+    $('.control__prev-game').prop('href', '#' + numPrev);
+    $('.control__next-game').prop('href', '#' + numNext);
 
     // counter
-    $('.control__counter').text(parseInt(numStudent) + '/' + parseInt(students.length));
-}
-
-function watchSwf($target) {
-    // set the focus() once the $target loads
-    var swfChecker = setInterval(function() {
-        var $swf = $target;
-
-        if ($swf[0]) {
-            $swf.focus();
-            $('.game__alert').fadeOut('500');
-            clearInterval(swfChecker);
-        }
-    }, 250);
+    $('.control__counter').text(numStudent + '/' + students.length);
 }
